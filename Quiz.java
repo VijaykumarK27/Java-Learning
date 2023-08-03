f8f77ee1a771a51c44b2286ab27823f2bbe62684
VijaykumarK27 <vijaykumar.kadappagol2001@gmail.com>
2023-08-03 18:53:00 +0530

    txt

---
+++ Quiz.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Quiz {
+    public static void main(String[]ags){
+        String[] nums = new String[] {"10","21","01"};  
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+}
+}
